85855dc666b0a5533c35edd3898fbe6eaa5b0ff8
IamD96 <67230588+IamD96@users.noreply.github.com>
2026-05-07 11:40:44 +0530

    Inital Setup

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('campaignBtn').addEventListener('click', () => {
+  alert('Welcome to the Khazai 2026 movement!');
+});
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Thank you for supporting Khazai 2026!');
+});
